9bf0fe5bf53b46965638590a4e4743545c875012
sowndharya2007-SA <sowndharya2007@gmail.com>
2026-09-27 02:27:21 +0530

    Prepare TARVEX26 for deployment

--- backend/app.py
+++ backend/app.py
@@ -2224,9 +2224,13 @@ if __name__ == "__main__":
 
     print("=" * 58)
     print()
+    if __name__ == "__main__":
+     import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
